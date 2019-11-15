32b1c9147816bce5d7b86a14407ba208f9a27e2a
Monika Czernicka <monika.czernicka@gmail.com>
2019-11-15 16:49:08 +0100

    Some part of work - to be changed...

---
+++ spec/JobsBulletin/Domain/Service/OfferRequirementsMatcherSpec.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace spec\JobsBulletin\Domain\Service;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class OfferRequirementsMatcherSpec extends ObjectBehavior
+{
+    private const ABILITY_BIKE = 'a bike';
+    private const ABILITY_DRIVING_LICENSE = 'a driving license';
+
+    public function it_returns_true_when_any_requirement_is_not_given()
+    {
+        //Given
+        $abilities = [
+            self::ABILITY_BIKE,
+            self::ABILITY_DRIVING_LICENSE,
+        ];
+        $requirements = [];
+
+        //When
+        $this->isMatched($abilities, $requirements)
+        //Then
+            ->shouldReturn(true);
+    }
+
+    public function it_returns_true_when_any_ability_meets_single_requirement()
+    {
+        //Given
+        $abilities = [
+            self::ABILITY_BIKE,
+            self::ABILITY_DRIVING_LICENSE,
+        ];
+        $requirements = [
+            self::ABILITY_BIKE,
+        ];
+
+        //When
+        $this->isMatched($abilities, $requirements)
+            //Then
+            ->shouldReturn(true);
+    }
+
+    public function it_return_false_when_any_ability_does_not_meet_single_requirement()
+    {
+        //Given
+        $abilities = [
+            self::ABILITY_BIKE,
+            self::ABILITY_DRIVING_LICENSE,
+        ];
+        $requirements = [
+            'other_requirement',
+        ];
+
+        //When
+        $this->isMatched($abilities, $requirements)
+        //Then
+            ->shouldReturn(false);
+    }
+
+    public function it_returns_true_when_at_least_one_condition_of_multiple_interchangeable_requirements_is_met()
+    {
+        //Given
+        $abilities = [
+            self::ABILITY_BIKE,
+            self::ABILITY_DRIVING_LICENSE,
+        ];
+        $requirements = [
+            'or' => [
+                'other_requirement',
+                self::ABILITY_BIKE,
+            ],
+        ];
+
+        //When
+        $this->isMatched($abilities, $requirements)
+        //Then
+            ->shouldReturn(true);
+    }
+
+    public function it_informs_that_abilities_meet_requirements()
+    {
+        //Given
+        $abilities = [
+            self::ABILITY_BIKE,
+            self::ABILITY_DRIVING_LICENSE,
+        ];
+        $requirements = [
+            'or' => [
+                'a scooter',
+                self::ABILITY_BIKE,
+                [
+                    'and' => [
+                        'a motorcycle',
+                        self::ABILITY_DRIVING_LICENSE,
+                        'motorcycle insurance',
+                    ],
+                ],
+            ]
+        ];
+
+        //When
+        $this->isMatched($abilities, $requirements)
+        //Then
+            ->shouldReturn(true);
+    }
+}
---
+++ src/JobsBulletin/Domain/Service/OfferRequirementsMatcher.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JobsBulletin\Domain\Service;
+
+class OfferRequirementsMatcher implements RequirementsMatcher
+{
+    public function isMatched(array $abilities, array $requirements): bool
+    {
+        if (!$requirements) {
+            return true;
+        }
+
+        $requirements
+
+        var_dump(
+            \array_keys($requirements),
+            in_array(['or'], \array_keys($requirements))
+        );
+        if (count($requirements) == 1 && !in_array(['or'], \array_keys($requirements))) {
+            return in_array($requirements[0], $abilities);
+        }
+        if (\array_keys($requirements, 'or')) {
+            $orRequirements = $requirements['or'];
+            foreach ($orRequirements as $requirement) {
+                if (in_array($requirement, $abilities)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
