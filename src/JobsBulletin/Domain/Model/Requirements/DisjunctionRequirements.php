039b9e193e9812e6957eb282f3f89a39f623ccbe
Monika Czernicka <monika.czernicka@gmail.com>
2019-11-16 00:30:18 +0100

    Added Requirements for disjunction (OR)

---
+++ src/JobsBulletin/Domain/Model/Requirements/DisjunctionRequirements.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JobsBulletin\Domain\Model\Requirements;
+
+class DisjunctionRequirements implements Requirements
+{
+    /**
+     * @var Requirements[]
+     */
+    private $requirements;
+
+    public function __construct(array $requirements)
+    {
+        $this->requirements = $requirements;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function areMet(array $abilities): bool
+    {
+        foreach ($this->requirements as $requirement) {
+            if ($requirement->areMet($abilities)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
