5dd6c8da545b5ab691a931e7cf39c6854b406e7d
Monika Czernicka <monika.czernicka@gmail.com>
2019-11-16 00:22:49 +0100

    Added Requirements for conjunction (AND)

---
+++ spec/JobsBulletin/Domain/Model/Requirements/ConjunctionRequirementsSpec.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace spec\JobsBulletin\Domain\Model\Requirements;
+
+use JobsBulletin\Domain\Model\Requirements\Requirements;
+use PhpSpec\ObjectBehavior;
+
+class ConjunctionRequirementsSpec extends ObjectBehavior
+{
+    private const ABILITIES = ['some abilities'];
+
+    public function it_returns_true_when_all_requirements_are_met(
+        Requirements $firstRequirement,
+        Requirements $secondRequirement,
+        Requirements $thirdRequirement
+    ) {
+        //Given
+        $this->beConstructedWith([
+            $firstRequirement,
+            $secondRequirement,
+            $thirdRequirement,
+        ]);
+
+        $firstRequirement->areMet(self::ABILITIES)->willReturn(true);
+        $secondRequirement->areMet(self::ABILITIES)->willReturn(true);
+        $thirdRequirement->areMet(self::ABILITIES)->willReturn(true);
+
+        //When
+        $this->areMet(self::ABILITIES)
+        //Then
+            ->shouldReturn(true);
+    }
+
+    public function it_returns_false_when_any_requirements_are_not_met(
+        Requirements $firstRequirement,
+        Requirements $secondRequirement,
+        Requirements $thirdRequirement
+    ) {
+        //Given
+        $this->beConstructedWith([
+            $firstRequirement,
+            $secondRequirement,
+            $thirdRequirement
+        ]);
+
+        $firstRequirement->areMet(self::ABILITIES)->willReturn(true);
+        $secondRequirement->areMet(self::ABILITIES)->willReturn(false);
+        $thirdRequirement->areMet(self::ABILITIES)->willReturn(true);
+
+        //When
+        $this->areMet(self::ABILITIES)
+        //Then
+            ->shouldReturn(false);
+    }
+}
